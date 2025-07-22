29c851392fd73d1d555b2fb6c74382c6fd87f8a1
Mariam Kvantaliani <47283342+mariamikv@users.noreply.github.com>
2025-07-23 01:39:34 +0400

    chore: add base multi-module project structure

--- app/src/main/java/com/mariam/android/challenge/ui/theme/Theme.kt
+++ app/src/main/java/com/mariam/android/challenge/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.mariam.android.challenge.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,19 @@ dependencyResolutionManagement {
 
 rootProject.name = "Android Code Challenge"
 include(":app")
- 
+include(":core")
+project(":core").projectDir = File("core")
+include(":core:data")
+project(":core:data").projectDir = File("core/data")
+include(":core:ui")
+project(":core:ui").projectDir = File("core/ui")
+include(":core:domain")
+project(":core:domain").projectDir = File("core/domain")
+include(":core:navigation")
+project(":core:navigation").projectDir = File("core/navigation")
+include(":feature")
+project(":feature").projectDir = File("feature")
+include(":feature:home")
+project(":feature:home").projectDir = File("feature/home")
+include(":feature:details")
+project(":feature:details").projectDir = File("feature/details")
